738cbad37387670088f30e14a9fba7f304115bef
Brenda <brendamitto@gmail.com>
2025-11-18 10:52:15 -0300

    feat - Preparacao do ambiente

--- sistema_adocao/campanha/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CampanhaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'campanha'
--- sistema_adocao/pagamento_doacao/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagamentoDoacaoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pagamento_doacao'
