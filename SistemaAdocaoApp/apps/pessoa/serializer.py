86debecfb0d34bd124a35dc0d99dda1ffd80e7e0
Brenda <brendamitto@gmail.com>
2025-11-19 08:24:15 -0300

    fix - Ajuste da configuracao de 'pessoa/serializer/2'

--- SistemaAdocaoApp/apps/pessoa/serializer.py
+++ SistemaAdocaoApp/apps/pessoa/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializer
 from .models import pessoa
 
 class pessoaSerializer(serializer.ModelSerializer):
+    
     class Meta:
         model = pessoa
         fields = '__all__'
