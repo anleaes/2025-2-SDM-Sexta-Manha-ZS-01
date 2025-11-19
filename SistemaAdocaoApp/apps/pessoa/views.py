09f3945778840634a2a45789e8fb441bd98dbfef
Brenda <brendamitto@gmail.com>
2025-11-19 08:32:30 -0300

    feat - Imports da configuracao de 'pessoa/views'

--- SistemaAdocaoApp/apps/pessoa/views.py
+++ SistemaAdocaoApp/apps/pessoa/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .models import pessoa
 from .serializer import pessoaSerializer
+from rest_framework import viewsets 
 
 # Create your views here.
-class pessoaViewSet(viewsets.ModelViewSet):
-    queryset = pessoa.objects.all()
-    serializer_class = pessoaSerializer 
-    
