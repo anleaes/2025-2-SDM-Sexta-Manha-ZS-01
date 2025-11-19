d0cd546eb3ac7e46e6caea75c4dd140aa0a6280a
Brenda <brendamitto@gmail.com>
2025-11-19 08:33:37 -0300

    feat - Adicao das classes em 'pessoa/views'

--- SistemaAdocaoApp/apps/pessoa/views.py
+++ SistemaAdocaoApp/apps/pessoa/views.py
@@ -4,3 +4,6 @@ from .serializer import pessoaSerializer
 from rest_framework import viewsets 
 
 # Create your views here.
+class pessoaViewSet(viewsets.ModelViewSet):
+    queryset = pessoa.objects.all()
+    serializer_class = pessoaSerializer 
