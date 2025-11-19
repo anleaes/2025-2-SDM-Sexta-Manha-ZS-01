2b7b1bdfda9684e3833c27ec57aee98be549902c
Brenda <brendamitto@gmail.com>
2025-11-19 09:40:02 -0300

    feat - conclusao do serializer/ong

--- SistemaAdocaoApp/apps/ong/serializer.py
+++ SistemaAdocaoApp/apps/ong/serializer.py
@@ -1,4 +1,8 @@
 from .models import ONG
 from rest_framework import serializer
 
-
+class ONGSerializer(serializer.ModelSerializer):
+    class Meta:
+        
+        model = ONG
+        fields = '__all__'
