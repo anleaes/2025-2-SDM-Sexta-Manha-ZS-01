d6841a891727d3c5ca2dba7cb358bc97d22f6509
Brenda <brendamitto@gmail.com>
2025-11-20 15:50:42 -0300

    imports em monitoramento/views

--- SistemaAdocaoApp/apps/monitoramento/views.py
+++ SistemaAdocaoApp/apps/monitoramento/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import MonitoramentoPosAdocao
+from .serializer import MonitoramentoPosAdocaoSerializer
 # Create your views here.
