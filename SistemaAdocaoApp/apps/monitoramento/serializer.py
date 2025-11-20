8d35aa54ed9e6af743e449fa41c37f7bc7a1be76
Brenda <brendamitto@gmail.com>
2025-11-20 15:44:04 -0300

    feat - conclusao do serializer/monitoramento-criacao da classe

--- SistemaAdocaoApp/apps/monitoramento/serializer.py
+++ SistemaAdocaoApp/apps/monitoramento/serializer.py
@@ -1,3 +1,8 @@
 from .models import MonitoramentoPosAdocao
 from rest_framework import serializers
 
+class MonitoramentoPosAdocaoSerializer(serializers.ModelSerializer):
+   
+    class Meta:
+        model = MonitoramentoPosAdocao
+        fields = '__all__'
