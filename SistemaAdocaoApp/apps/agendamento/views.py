93f589635776fbbbd4690632a0304ad81046db59
Brenda <brendamitto@gmail.com>
2025-11-19 23:06:02 -0300

    feat - criacao e imports de views/AgendamentoAdocao

--- SistemaAdocaoApp/apps/agendamento/views.py
+++ SistemaAdocaoApp/apps/agendamento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AgendamentoVisita
+from .serializer import AgendamentoVisitaSerializer
 
 # Create your views here.
