c306d2d2f576a27ce065bf8bd44c7d5f0697750c
Brenda <brendamitto@gmail.com>
2025-11-20 00:15:55 -0300

    feat - conclusao do views/campanha - criacao da classe

--- SistemaAdocaoApp/apps/campanha/views.py
+++ SistemaAdocaoApp/apps/campanha/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Campanha
 from .serializer import CampanhaSerializer
+
 # Create your views here.
+class CampanhaViewSet(viewsets.ModelViewSet):
+    queryset = Campanha.objects.all()
+    serializer_class = CampanhaSerializer
