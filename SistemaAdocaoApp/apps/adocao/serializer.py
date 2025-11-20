8b2eda016e2f9633206e7e678c9a3a2dd83309a7
Brenda <brendamitto@gmail.com>
2025-11-19 22:06:01 -0300

    feat - conclusao do serializer/adocao - criacao da classe

--- SistemaAdocaoApp/apps/adocao/serializer.py
+++ SistemaAdocaoApp/apps/adocao/serializer.py
@@ -1,2 +1,7 @@
-from .models import Animal
+from .models import Adocao
 from rest_framework import serializers
+
+class AdocaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Adocao
+        fields = '__all__'
