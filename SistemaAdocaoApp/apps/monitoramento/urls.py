d99f8998d7f04a14968a818bc07961e48c45c6af
Brenda <brendamitto@gmail.com>
2025-11-20 16:02:30 -0300

    Conclusao de monitoramento/urls, configuracao no banco e inclusao no gitignore

--- SistemaAdocaoApp/apps/monitoramento/urls.py
+++ SistemaAdocaoApp/apps/monitoramento/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'monitoramentos'
+
+router = routers.DefaultRouter()
+router.register('', views.MonitoramentoPosAdocaoViewSet, basename='monitoramentos')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
