403d9e7b38f8fa59f2d8e731da299e59fe5e7d6c
Brenda <brendamitto@gmail.com>
2025-11-19 23:31:18 -0300

    fix - conclusao do urls/AgendamentoVisita - criacao no banco

--- SistemaAdocaoApp/apps/agendamento/urls.py
+++ SistemaAdocaoApp/apps/agendamento/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'agendamentos'
+app_name = 'agendamento'
 
 router = routers.DefaultRouter()
-router.register('', views.AgendamentoVisitaViewSet, basename='agendamentos')
+router.register('', views.AgendamentoVisitaViewSet, basename='agendamento')
 
 urlpatterns = [
     path('', include(router.urls) )
