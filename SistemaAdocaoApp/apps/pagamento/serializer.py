4b373eee2c730cd8326175fd60aa2aa9805d3e76
Brenda <brendamitto@gmail.com>
2025-11-20 13:35:06 -0300

    feat - conclusao do serializer/pagamento- criacao da classe

--- SistemaAdocaoApp/apps/pagamento/serializer.py
+++ SistemaAdocaoApp/apps/pagamento/serializer.py
@@ -1,2 +1,7 @@
-from .models import PagamentoDoacao
+from .models import Pagamento
 from rest_framework import serializers
+
+class PagamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pagamento
+        fields = '__all__'
